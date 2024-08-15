3d74a248fa8857d35cbf860ad73513f47a3cf418
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-08-15 11:31:50 -0300

    ok

---
+++ src/commands/decode/BinaryCommand.js
@@ -0,0 +1,12 @@
+const Command = require("../../base/Commandand");
+
+module.exports = new Command("decode binary", async ({ interaction }) => {
+  const text = interaction.options.getString("text", true);
+
+  interaction.editReply(
+    `\`\`\`${text
+      .split(" ")
+      .map((x) => String.fromCharCode(parseInt(x, 2)))
+      .join("")}\`\`\``
+  );
+});
---
+++ src/commands/decode/MorseCommand.js
@@ -0,0 +1,18 @@
+const Command = require("../../base/Commandand");
+const Morse = require("../../../data/morse.json");
+
+module.exports = new Command("decode morse", async ({ interaction }) => {
+  const text = interaction.options.getString("text", true);
+  const morse = {};
+
+  for (const [key, val] of Object.entries(Morse)) {
+    morse[val] = key;
+  }
+
+  interaction.editReply(
+    `\`\`\`${text
+      .split(" ")
+      .map((c) => (morse[c] ? morse[c] : "?"))
+      .join("")}\`\`\``
+  );
+});
