c628a7cc70fb8dfc5d2b9752b1f953bfe99de802
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-08-15 00:23:42 -0300

    status do bot

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,4 +2,8 @@ const Event = require("../base/Event");
 
 module.exports = new Event("ready", async (app) => {
   console.log(`${app.user.username} está online!`.bgGreen);
+
+  app.user.setPresence({
+    activities: [{ type: 1, name: "Em breve estárei de volta ;)" }],
+  });
 });
