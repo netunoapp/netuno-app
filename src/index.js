88f2ac6acf59c03bb72d2875017255628d781cee
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-08-15 00:58:16 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ const app = new App({
   intents: ["Guilds", "GuildMembers"],
 });
 
-app.on("interactionCreate", async (interaction) => {
-  if(interaction.isChatInputCommand()){
-    const name = interaction.commandName;
-  }
-});
-
 app.login(process.env.TOKEN).then(async () => {
   const handler = new Handler(app);
 
