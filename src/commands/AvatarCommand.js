336e8431634fd32f983b62e8c5be2fdda9f3ea2a
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-08-15 11:07:45 -0300

    ok

--- src/commands/AvatarCommand.js
+++ src/commands/AvatarCommand.js
@@ -1,21 +1,44 @@
-const { EmbedBuilder } = require("discord.js");
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  parseEmoji,
+} = require("discord.js");
 const Command = require("../base/Command");
+const emojis = require("../../data/emojis.json");
 
-module.exports = new Command("user avatar", async ({ interaction }) => {
+module.exports = new Command("user avatar", async ({ t, interaction }) => {
   const user = interaction.options.getUser("user", false) ?? interaction.user;
-  const avatar = user.displayAvatarURL({
+  const url = user.displayAvatarURL({
     size: 4096,
     extension: user.avatar && user.avatar.includes("a_") ? "gif" : "png",
   });
 
-  console.log(avatar);
+  if (url === null) {
+    interaction.editReply(`${emojis.no} **│** ${t["!user_avatar"]}`);
+  } else {
+    const downloadEmoji = await parseEmoji(emojis.download);
 
-  interaction.editReply({
-    embeds: [
-      new EmbedBuilder()
-        .setColor("Blurple")
-        .setImage(avatar)
-        .setTitle(user.globalName ?? user.username)
-    ],
-  });
+    interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Blurple")
+          .setImage(url)
+          .setTitle(user.globalName ?? user.username),
+      ],
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setURL(url)
+            .setLabel(t.download)
+            .setEmoji({
+              id: downloadEmoji.id,
+              name: downloadEmoji.name,
+            })
+            .setStyle(ButtonStyle.Link)
+        ),
+      ],
+    });
+  }
 });
--- src/commands/BannerCommand.js
+++ src/commands/BannerCommand.js
@@ -1,22 +1,47 @@
-const { EmbedBuilder } = require("discord.js");
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  parseEmoji,
+} = require("discord.js");
 const Command = require("../base/Command");
+const emojis = require("../../data/emojis.json");
+const getUser = require("../helpers/getUser");
 
-module.exports = new Command("user banner", async ({ interaction }) => {
-  const user = interaction.options.getUser("user", false) ?? interaction.user;
-  const banner = user.bannerURL({
-    size: 4096,
-    forceStatic: true,
-    extension: user.banner && user.banner.includes("a_") ? "gif" : "png",
-  });
+module.exports = new Command("user banner", async ({ t, interaction }) => {
+  const userOption = interaction.options.getUser("user", false) ?? interaction.user;
+  const user = await getUser(userOption.id);
+  const url = user.banner
+    ? `https://cdn.discordapp.com/banners/${user.id}/${user.banner}.${user.banner.includes("a_") ? "gif" : "png"}?size=4096`
+    : null;
 
-  console.log(banner);
+  console.log(user.banner, url);
 
-  interaction.editReply({
-    embeds: [
-      new EmbedBuilder()
-        .setColor("Blurple")
-        .setImage(banner)
-        .setTitle(user.globalName ?? user.username)
-    ],
-  });
+  if (url === null) {
+    interaction.editReply(`${emojis.no} **│** ${t["!user_banner"]}`);
+  } else {
+    const downloadEmoji = await parseEmoji(emojis.download);
+
+    interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Blurple")
+          .setImage(url)
+          .setTitle(user.globalName ?? user.username),
+      ],
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setURL(url)
+            .setLabel(t.download)
+            .setEmoji({
+              id: downloadEmoji.id,
+              name: downloadEmoji.name,
+            })
+            .setStyle(ButtonStyle.Link)
+        ),
+      ],
+    });
+  }
 });
