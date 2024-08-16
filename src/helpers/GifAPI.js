787ff88840aa3a2e451a7eed62527429ef52ec2e
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-08-16 19:44:45 -0300

    ok

--- src/helpers/GifAPI.js
+++ src/helpers/GifAPI.js
@@ -1,43 +1,34 @@
-
 const Axios = require("axios");
 const base = "https://nekos.life/api/v2/img/";
 
 module.exports = class GifAPI {
-    constructor() { }
+  static async cat() {
+    return await Axios.get(`${base}/meow`).then((res) => res.data.url);
+  }
 
-    getCat() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get(`${base}/meow`).then((res) => res.data.url);
-        });
-    }
-    getDog() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get("https://dog.ceo/api/breeds/image/random").then((res) => res.data.message);
-        });
-    }
-    getHug() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get(`${base}/hug`).then((res) => res.data.url);
-        });
-    }
-    getKiss() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get(`${base}/kiss`).then((res) => res.data.url);
-        });
-    }
-    getPat() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get(`${base}/pat`).then((res) => res.data.url);
-        });
-    }
-    getSlap() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get(`${base}/slap`).then((res) => res.data.url);
-        });
-    }
-    getTickle() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return axios_1.default.get(`${base}/tickle`).then((res) => res.data.url);
-        });
-    }
-}
+  static async dog() {
+    return await Axios.get("https://dog.ceo/api/breeds/image/random").then(
+      (res) => res.data.message
+    );
+  }
+
+  static async hug() {
+    return await Axios.get(`${base}/hug`).then((res) => res.data.url);
+  }
+
+  static async kiss() {
+    return await Axios.get(`${base}/kiss`).then((res) => res.data.url);
+  }
+
+  static async pat() {
+    return await Axios.get(`${base}/pat`).then((res) => res.data.url);
+  }
+
+  static async slap() {
+    return await Axios.get(`${base}/slap`).then((res) => res.data.url);
+  }
+
+  static async tickle() {
+    return await Axios.get(`${base}/tickle`).then((res) => res.data.url);
+  }
+};
