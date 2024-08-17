505c2493cde00f13fd04cbb71f048de1dca3bbd8
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-08-17 01:53:01 -0300

    ok

--- src/global/commands/RandomCommand.js
+++ src/global/commands/RandomCommand.js
@@ -1,13 +1,10 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const oceanic_js_1 = require("oceanic.js");
-const Command_1 = __importDefault(require("../base/Command"));
-const EphemeralOption_1 = __importDefault(require("../options/EphemeralOption"));
+const { ApplicationCommandOptionTypes, ApplicationIntegrationTypes, InteractionContextTypes, ApplicationCommandTypes } = require("oceanic.js");
+const Command = require("../base/Command");
+const EphemeralOption = require("../options/EphemeralOption");
+
 function createCommand() {
     const options = [];
+
     for (let i = 0; i < 23; i++) {
         options.push({
             name: `choice${i + 1}`,
@@ -19,61 +16,32 @@ function createCommand() {
             descriptionLocalizations: {
                 "pt-BR": "Escolha na lista de escolhas.",
             },
-            type: oceanic_js_1.ApplicationCommandOptionTypes.STRING,
+            type: ApplicationCommandOptionTypes.STRING,
         });
     }
-    options.push((0, EphemeralOption_1.default)(false));
-    return (0, Command_1.default)({
+
+    options.push(EphemeralOption(false));
+
+    return Command({
         name: "random",
         nameLocalizations: {
             "pt-BR": "aleatório",
         },
         description: "undefined",
         integrationTypes: [
-            oceanic_js_1.ApplicationIntegrationTypes.GUILD_INSTALL,
-            oceanic_js_1.ApplicationIntegrationTypes.USER_INSTALL,
+            ApplicationIntegrationTypes.GUILD_INSTALL,
+            ApplicationIntegrationTypes.USER_INSTALL,
         ],
         contexts: [
-            oceanic_js_1.InteractionContextTypes.BOT_DM,
-            oceanic_js_1.InteractionContextTypes.GUILD,
-            oceanic_js_1.InteractionContextTypes.PRIVATE_CHANNEL,
+            InteractionContextTypes.BOT_DM,
+            InteractionContextTypes.GUILD,
+            InteractionContextTypes.PRIVATE_CHANNEL,
         ],
-        type: oceanic_js_1.ApplicationCommandTypes.CHAT_INPUT,
+        type: ApplicationCommandTypes.CHAT_INPUT,
         options: [
             {
-                type: oceanic_js_1.ApplicationCommandOptionTypes.SUB_COMMAND_GROUP,
-                name: "animal",
-                description: "undefined",
-                options: [
-                    {
-                        type: oceanic_js_1.ApplicationCommandOptionTypes.SUB_COMMAND,
-                        name: "cat",
-                        nameLocalizations: {
-                            "pt-BR": "gato",
-                        },
-                        description: "See a cat.",
-                        descriptionLocalizations: {
-                            "pt-BR": "Veja um gato.",
-                        },
-                        options: [(0, EphemeralOption_1.default)(false)]
-                    },
-                    {
-                        type: oceanic_js_1.ApplicationCommandOptionTypes.SUB_COMMAND,
-                        name: "dog",
-                        nameLocalizations: {
-                            "pt-BR": "cachorro",
-                        },
-                        description: "See a dog.",
-                        descriptionLocalizations: {
-                            "pt-BR": "Veja um cachorro.",
-                        },
-                        options: [(0, EphemeralOption_1.default)(false)]
-                    },
-                ],
-            },
-            {
                 options: options,
-                type: oceanic_js_1.ApplicationCommandOptionTypes.SUB_COMMAND,
+                type: ApplicationCommandOptionTypes.SUB_COMMAND,
                 name: "choice",
                 nameLocalizations: {
                     "pt-BR": "escolha",
